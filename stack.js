ebba5f8f6a6b98b3a4dcccef709ec57e32ba220e
shymaSane <shymaSane1@gmail.com>
2017-11-09 10:40:20 +0200

    attempt to make storage as object

--- stack.js
+++ stack.js
@@ -20,20 +20,44 @@
 // test.push(1);
 
 
-class Stack{
-    constructor(){
-     this.strage = {}
-     this.counter = 0
-    }
-    push(val){
-        this.storage[this.counter++] = val
-        return this.storage
-    }
-    pop(){ 
-        let pop = delete this.storage[this.counter]
-        return pop
-    }
-    size(){
-        return counter
-    }
+// class Stack{
+//     constructor(){
+//      this.strage = {}
+//      this.counter = 0
+//     }
+//     push(val){
+//         this.storage[this.counter++] = val
+//         return this.storage
+//     }
+//     pop(){ 
+//         let pop = delete this.storage[this.counter]
+//         return pop
+//     }
+//     size(){
+//         return counter
+//     }
+// }
+
+var Stack = function (){
+ this.storage ={}
+ this.counter = 0
 }
+
+
+Stack.prototype.push = function(val){
+    this.storage[this.counter++] = val
+    return this.storage
+}
+
+Stack.prototype.pop = function() {
+   var val = delete this.storage[this.counter]
+   return val
+}
+
+Stack.prototype.size = function(){
+    return counter
+ }
+
+
+
+
