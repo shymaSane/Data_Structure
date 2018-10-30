f3ee6359e7978aba05d9ea2f98b68b17c14f56b8
shymaSane <shymaSane1@gmail.com>
2018-10-30 11:20:10 +0200

    added node and singly linked lists classes

---
+++ Singly_Linked_Lists.js
@@ -0,0 +1,17 @@
+//this class to init new Node that ll be added to the new linked lists
+class Node{
+	constructor(val){
+		this.val = val;
+		this.next = null;
+	}
+}
+
+// this class to init new Linked Lists to add nodes to it
+
+class SinglyLinkedLists{
+    constructor(){
+        this.length = 0;
+        this.head = null;
+        this.tail = null
+    }
+}
