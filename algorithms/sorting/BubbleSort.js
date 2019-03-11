b473a7930dfab3f03dada8c62b0b97fcb88cb179
shymaSane <shymaSane1@gmail.com>
2019-03-11 18:46:19 +0200

    fix: bubble sort

--- algorithms/sorting/BubbleSort.js
+++ algorithms/sorting/BubbleSort.js
@@ -3,20 +3,42 @@
 //it cmpares the current index to the next index then swap
 //since we decided what is the bigger value the length of the loop become shorter
 
-//swaping function using ES6:
+// swaping function using ES6:
 const swap = (arr, index1, index2) => {
     [arr[index1], arr[index2]] = [arr[index2], arr[index1]]
 }
 
-const bubbleSort = (arr, n = 1) => {
-    if(n <= arr.length){
-        for (let i = 0; i < (arr.length-n) ; i++) {
-            if(arr[i] > arr[i+1]){
-                swap(arr, i, i+1)
-            } 
+// const bubbleSort = (arr, n = 1) => {
+//     if(n <= arr.length){
+//         for (let i = 0; i < (arr.length-n) ; i++) {
+//             if(arr[i] > arr[i+1]){
+//                 swap(arr, i, i+1)
+//                 console.log(arr)
+//             } 
            
+//         }
+//         n +=1 
+//         bubbleSort(arr, n)
+//     }
+// }
+
+function bubbleSort(items){
+
+    var len = items.length,
+        i, j, stop;
+
+    for (i=0; i < len; i++){
+        for (j=0, stop=len-i; j < stop; j++){
+            if (items[j] > items[j+1]){
+                swap(items, j, j+1);
+                console.log(items)
+            }
         }
-        n +=1 
-        bubbleSort(arr, n)
     }
+
+    return items;
 }
+
+let xx = [1, -2, 4, -3, 0, -5, 5, 3, 2, -1, -4]
+
+console.log(bubbleSort(xx))
--- algorithms/sorting/selectionSort.js
+++ algorithms/sorting/selectionSort.js
@@ -2,19 +2,23 @@
 //we compare all values to find the minmum one 
 // if a<b we save a in min variable 
 
-
+//l=0  is the postion where we want to start search
 const selectionSort = (arr, l=0) => {
-    if(l < arr.length){
-        let min = arr[l];
-        for (let i = l+1; i < arr.length; i++) {
-            if(arr[i] < min) {
-                arr[l] = arr[i]
-                arr[i] = min
+            let min = arr[l];
+            for (let i = l+1; i < arr.length; i++) {
+                if(arr[i] < min) {
+                   min = arr[i]
+                   arr[i] = arr[l]
+                }
             }
-        
-        }
-        l +=1
-        selectionSort(arr, l)
-    }
-    
+
+            if(l < arr.lenght){
+                l += 1
+                selectionSort(arr, l)
+            }
+            
 }
+
+let x = [1, -2, 4, -3, 0, -5, 5, 3, 2, -1, -4]
+
+console.log(selectionSort(x))
