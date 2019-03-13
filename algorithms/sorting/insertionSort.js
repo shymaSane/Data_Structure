1d9f51fb5cad1f426c63a4169b5e89cdfcb1d2e7
shymaSane <shymaSane1@gmail.com>
2019-03-13 19:06:35 +0200

    add: insertion sort

---
+++ algorithms/sorting/insertionSort.js
@@ -0,0 +1,16 @@
+//devided sort and unsorted array 
+
+const insertionSort = (arr, l = 1) =>{
+    for(let i = l-1; i >= 0 && arr[i] > arr[i+1]; i--){
+        [arr[i+1], arr[i]] = [arr[i], arr[i+1]]
+    }
+
+    if(l < arr.length){
+        l += 1;
+        insertionSort(arr, l)
+    }
+    return arr
+}
+
+arr = [3, 5, 2, 4, 1] 
+console.log(insertionSort(arr))
